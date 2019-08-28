006e08da85dd04cd847d0a336055164f1208540a
pei luo <alexluolibra@hotmail.com>
2019-08-28 23:35:29 +1000

    structure changes

--- src/index.js
+++ src/index.js
@@ -3,19 +3,43 @@ import ReactDOM from "react-dom"
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    };
+  
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={()=> 
+          this.props.onClick()} >
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
     }
+
+    handleClick(i){
+      const squares = 
+      this.state.squares.slice();
+      squares[i] = 'y';
+      this.setState({squares: squares});
+    }
+
+    renderSquare(i) {
+      return (<Square value={this.state.squares[i]} 
+              onClick={() => this.handleClick(i)}
+      />);
+      //为什么这么写，有什么意义
+    }
+
   
     render() {
       const status = 'Next player: X';
